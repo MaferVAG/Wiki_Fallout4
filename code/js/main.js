9b5547b072b556b2fa62caf9d4f13f149d96daaa
Mafer Vásquez <143749653+MaferVAG@users.noreply.github.com>
2024-05-21 10:41:32 -0600

    Cambios buscador

--- code/js/main.js
+++ code/js/main.js
@@ -1,1 +1,19 @@
 console.log("Hola Mundo desde 11ty 🦡🎈");
+
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").forEach(nombre =>{
+
+          nombre.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?nombre.classList.remove("filtro")
+            :nombre.classList.add("filtro")
+      })
+
+  }
+
+
+})
